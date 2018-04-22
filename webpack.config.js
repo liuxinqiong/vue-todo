3ef03ac1d53769aaabb3465ad0241bd4da3dc6c1
liuxinqiong <372513646@qq.com>
2018-04-22 20:48:10 +0800

    optimize webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,13 @@ const path = require('path')
 const htmlWebpackPlugin = require('html-webpack-plugin')
 const webpack = require('webpack')
 const isDev = process.env.NODE_ENV === 'development'
+const extractPlugin = require('extract-text-webpack-plugin')
 
 const config = {
+    target: 'web',
     entry: path.join(__dirname, 'src/index.js'),
     output: {
-        filename: 'bundle.js',
+        filename: 'bundle.[hash:8].js',
         path: path.join(__dirname, 'dist')
     },
     module: {
@@ -29,20 +31,6 @@ const config = {
                 ]
             },
             {
-                test: /\.styl$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            sourceMap: true
-                        }
-                    },
-                    'stylus-loader'
-                ]
-            },
-            {
                 test: /\.(gif|png|jpg|jpeg|svg)$/i,
                 use: [
                     {
@@ -62,11 +50,14 @@ const config = {
                 NODE_ENV: isDev ? '"development"' : '"production"'
             }
         }),
-        new htmlWebpackPlugin()
+        new htmlWebpackPlugin({
+            template: 'index.html'
+        })
     ]
 }
 
 if (isDev) {
+    config.devtool = '#cheap-module-eval-source-map';
     config.devServer = {
         port: 8000,
         // 可以通过localhost，127.0.0.1，主要还包括内网地址可以访问
@@ -85,7 +76,76 @@ if (isDev) {
     config.plugins.push(
         new webpack.HotModuleReplacementPlugin(),
         new webpack.NoEmitOnErrorsPlugin()
+    );
+    config.module.rules.push({
+        test: /\.styl$/,
+        use: [
+            'style-loader',
+            'css-loader',
+            {
+                loader: 'postcss-loader',
+                options: {
+                    sourceMap: true
+                }
+            },
+            'stylus-loader'
+        ]
+    })
+} else {
+    config.entry = {
+        app: path.join(__dirname, 'src/index.js'),
+        // 指定类库
+        vendor: ['vue']
+    }
+    config.output.filename = '[name].[chunkhash:8].js'
+    config.module.rules.push({
+        test: /\.styl$/,
+        use: extractPlugin.extract({
+            fallback: 'style-loader',
+            use: [
+                'css-loader',
+                {
+                    loader: 'postcss-loader',
+                    options: {
+                        sourceMap: true
+                    }
+                },
+                'stylus-loader'
+            ]
+        })
+    })
+    config.plugins.push(
+        new extractPlugin('styles.[chunkhash:8].css')
+        // // 将类库文件单独打包出来
+        // new webpack.optimize.CommonsChunkPlugin({
+        //     name: 'vendor'
+        // })
+
+        // webpack相关的代码单独打包
+        // new webpack.optimize.CommonsChunkPlugin({
+        //     name: 'runtime'
+        // })
     )
+    config.optimization = {
+        splitChunks: {
+            cacheGroups: {                  // 这里开始设置缓存的 chunks
+                commons: {
+                    chunks: 'initial',      // 必须三选一： "initial" | "all" | "async"(默认就是异步)
+                    minSize: 0,             // 最小尺寸，默认0,
+                    minChunks: 2,           // 最小 chunk ，默认1
+                    maxInitialRequests: 5   // 最大初始化请求数量，默认1
+                },
+                vendor: {
+                    test: /node_modules/,   // 正则规则验证，如果符合就提取 chunk
+                    chunks: 'initial',      // 必须三选一： "initial" | "all" | "async"(默认就是异步)
+                    name: 'vendor',         // 要缓存的 分隔出来的 chunk 名称
+                    priority: 10,           // 缓存组优先级
+                    enforce: true
+                }
+            }
+        },
+        runtimeChunk: true
+    }
 }
 
 module.exports = config
